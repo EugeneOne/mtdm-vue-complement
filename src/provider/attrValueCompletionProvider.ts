ace230a3300fbcc28499b6d303a1dd2eb0245a67
wanyuqing <wyq_life@yeah.net>
2021-01-11 20:29:10 +0800

    develop

---
+++ src/provider/attrValueCompletionProvider.ts
@@ -0,0 +1,8 @@
+import {
+    CancellationToken, CompletionItemProvider, ProviderResult,
+    TextDocument, Position, CompletionItem, CompletionList, CompletionItemKind,
+    SnippetString
+} from 'vscode';
+import mtdConfig from '../config/index'
+
+export default class AttrValue
